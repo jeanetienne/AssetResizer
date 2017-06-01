03a72123a16a5faf67d290d7bc61dc03e9a7b86c
Jean-Étienne <git@jeanetienne.net>
2017-06-01 14:39:46 +1000

    Basic Xcode project setup

---
+++ AssetResizer/AssetResizer.h
@@ -0,0 +1,19 @@
+//
+//  AssetResizer.h
+//  AssetResizer
+//
+//  Created by Jean-Étienne on 1/6/17.
+//  Copyright © 2017 Jean-Étienne. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for AssetResizer.
+FOUNDATION_EXPORT double AssetResizerVersionNumber;
+
+//! Project version string for AssetResizer.
+FOUNDATION_EXPORT const unsigned char AssetResizerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AssetResizer/PublicHeader.h>
+
+
