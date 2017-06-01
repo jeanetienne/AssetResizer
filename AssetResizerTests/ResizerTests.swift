fee6da067e1665054b4d112fd5a103d1f5784d8c
Jean-Étienne <git@jeanetienne.net>
2017-06-01 15:02:37 +1000

    Removing default Obj-C header and adding first class and tests

---
+++ AssetResizerTests/ResizerTests.swift
@@ -0,0 +1,25 @@
+//
+//  ResizerTests.swift
+//  AssetResizer
+//
+//  Created by Jean-Étienne on 1/6/17.
+//  Copyright © 2017 Jean-Étienne. All rights reserved.
+//
+
+import XCTest
+
+class ResizerTests: XCTestCase {
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
