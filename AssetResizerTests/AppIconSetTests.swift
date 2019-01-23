70ba73f3b41918e7d0f689530ebe400dedfaf27e
Jean-Étienne <git@jeanetienne.net>
2019-01-24 09:59:24 +1100

    Updating copyright notice

--- AssetResizerTests/AppIconSetTests.swift
+++ AssetResizerTests/AppIconSetTests.swift
@@ -1,9 +1,6 @@
 //
-//  AppIconSetTests.swift
 //  AssetResizer
-//
-//  Created by Jean-Étienne on 2/6/17.
-//  Copyright © 2017 Jean-Étienne. All rights reserved.
+//  Copyright © 2019 Jean-Étienne. All rights reserved.
 //
 
 import XCTest
